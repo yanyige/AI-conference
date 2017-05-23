6778f4a487399827e678dbd0590bcd13aab0bd48
yanyige <232218719@qq.com>
2017-05-23 12:15:21 +0800

    update some excerpt

--- src/components/Organization.vue
+++ src/components/Organization.vue
@@ -1,17 +1,17 @@
 <template>
 	<div class="organization">
 		<div class="title">
-	 		<p>问题的表示与算法 系列会议组织: https://cs.nenu.edu.cn</p>
+	 		<!-- <p>问题的表示与算法 系列会议组织: https://cs.nenu.edu.cn</p> -->
 	 	</div>
 		<div class="content">
 			<div>
-				<p>主办单位: <p class="indent1"><a href="http://www.nenu.edu.cn/">东北师范大学</a></p></p>
+				<p>主办单位: <p class="indent1"><a href="http://www.ios.ac.cn/wiki/%E9%A6%96%E9%A1%B5">中国科学院软件研究所</a></p><p class="indent1"><a href="http://www.nenu.edu.cn/">东北师范大学</a></p><p class="indent1"><a href="http://www.jlu.edu.cn/">吉林大学</a></p></p>
 			</div>
 			<div>
-				<p>协办单位: <p class="indent1"><a href="http://www.jlu.edu.cn/">吉林大学</a></p></p>
+				<p>协办单位: 暂无</p>
 			</div>
 			<div>
-				<p>会议主席: <p class="indent1"><a href="http://yinmh.nenu.edu.cn/">殷明浩</a> <a href="http://www.nenu.edu.cn/">东北师范大学</a></p></p>
+				<p>会议主席: 待定</p>
  			</div>
  			<div>
 				<p>[组委会联系方式]: <p class="indent1">通信地址：吉林省长春市南关区净月经济开发区净月大街2555号 计算机科学与技术学院</p></p>
