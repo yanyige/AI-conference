9bc8094ff690cdbc24a90ddec83642cf9733f56d
yanyige <232218719@qq.com>
2017-05-31 20:42:42 +0800

    update profiles

--- src/components/Professor.vue
+++ src/components/Professor.vue
@@ -1,10 +1,157 @@
 <template>
 	<div class="professor">
 		<div class="title">
+			邀请参会人员
+		</div>
+		<div class="title" style="font-size: 12px">
 			(按姓名拼音首字母排序)
 		</div>
+		<div class="school">北航大学</div>
+		<div class="content">
+			<div class="profile">许可</div>
+			<div class="titles">教授</div>
+		</div>
+		<div class="school">北京大学</div>
+		<div class="content">
+			<div class="profile">刘田</div>
+			<div class="titles">副教授</div>
+		</div>
+		<div class="school">University of Picardie Jules Verne</div>
+		<div class="content">
+			<div class="profile">Chu Min Li</div>
+			<div class="titles">教授</div>
+		</div>
+		<div class="school">Darwin University</div>
+		<div class="content">
+			<div class="profile">Peter Shaw</div>
+			<div class="titles">高级讲师</div>
+		</div>
+		<div class="school">中科院计算所</div>
+		<div class="content">
+			<div class="profile">刘兴武</div>
+			<div class="titles">副研究员</div>
+		</div>
+		<div class="content">
+			<div class="profile">罗川</div>
+			<div class="titles">助理研究员</div>
+		</div>
+		<div class="school">西安交通大学</div>
+		<div class="content">
+			<div class="profile">柯良军</div>
+			<div class="titles">副教授</div>
+		</div>
+		<div class="school">华中科技大学</div>
+		<div class="content">
+			<div class="profile">吕志鹏</div>
+			<div class="titles">教授</div>
+		</div>
+		<div class="school">渡合科技有限公司</div>
+		<div class="content">
+			<div class="profile">黄冲</div>
+		</div>
+		<div class="school">顺丰科技</div>
+		<div class="content">
+			<div class="profile">黄基锋</div>
+		</div>
+		<div class="school">中科院软件所</div>
+		<div class="content">
+			<div class="profile">张健</div>
+			<div class="titles">研究员</div>
+		</div>
+		<div class="content">
+			<div class="profile">马菲菲</div>
+			<div class="titles">副研究员</div>
+		</div>
+		<div class="content">
+			<div class="profile">蔡少伟</div>
+			<div class="titles">副研究员</div>
+		</div>
+		<div class="school">吉林大学</div>
+		<div class="content">
+			<div class="profile">欧阳丹彤</div>
+			<div class="titles">教授</div>
+			<div class="excerpt"><p>
+				国务院学位委员会学科评议组成员、中国计算机学会人工智能与模式识别专业委员会副主任、中国计算机学会理论计算机科学专业委员会委员、吉林省计算机学会副理事长；《计算机学报》编委、《软件学报》编委、《吉林大学学报(理学版)》编委。
+				<ul class="project">
+				<li>项目:</li>
+				<li>骨干参加国家级与省部级项目20余项，其中作为项目负责人或责任人承担并完成国家863重大项目2项、完成与主持国家自然科学基金项目共3项、教育部项目2项、中国博士后科学基金项目1项，骨干参加国家自然科学基金重大项目子课题1项。</li>
+				</ul>
+			</p></div>
+		</div>
+		<div class="content">
+			<div class="profile">李占山</div>
+			<div class="titles">教授</div>
+		</div>
+		<div class="content">
+			<div class="profile">张永刚</div>
+			<div class="titles">副教授</div>
+			<div class="excerpt"><p>
+				2007年至2009年为吉林大学数学研究所博士后，合作导师为高文杰教授，2009年至2010年在法国蒙彼利埃大学LIRMM实验室作访问学者，合作老师为国际著名人工智能专家Christian Bessiere教授。现为吉林大学计算机科学与技术学院副教授、硕士生导师。研究方向为人工智能与约束求解，发表论文30余篇，主持国家自然科学基金项目2项，吉林省青年科研基金项目1项，教育部基础科研业务费--学科前沿与交叉项目1项，横向项目2项。目前指导硕士研究生7名，已毕业2名
+				<ul class="project">
+				<li>项目:</li>
+				<li>1. 2008/01-2010/12，国家自然科学基金面上项目，“扩展规则推理方法研究（60773097）”</li>
+				<li>2. 2012/01-2015/12，国家自然科学基金面上项目，“基于自适应约束传播的约束求解方法研究（61170314）”</li>
+				<li>3. 2014/01-2017/12，国家自然科学基金面上项目，“结合自主搜索机制的约束求解方法研究（61373052）”</li>
+				<li>4. 2008/09-2010/12，吉林省青年科研基金项目，“基于推理的约束求解方法与应用研究（20080107）”</li>
+				</ul>
+			</p></div>
+		</div>
+		<div class="content">
+			<div class="profile">吕帅</div>
+			<div class="titles">副教授</div>
+			<div class="excerpt"><p>
+				主要研究领域为人工智能、智能规划与自动推理。累计承担国家自然科学基金项目等科研项目7项，参加政府间科技合作项目等科研项目32项。在《计算机学报》、《软件学报》、《计算机研究与发展》和《电子学报》等国内外期刊和会议上，累计发表学术论文63篇，其中：SCI和EI检索学术论文38篇。获得全国商业科技进步一等奖2项，吉林省科学技术进步三等奖2项。
+				<ul class="project">
+				<li>项目:</li>
+				<li>1. 国家自然科学基金：命题与模态逻辑的扩展规则推理与混合推理方法研究（61300049）</li>
+				<li>2. 教育部高等学校博士学科点专项科研基金：命题逻辑和模态逻辑的扩展规则推理方法研究（20120061120059）</li>
+				<li>3. 中国博士后科学基金面上资助项目：自动规划的逻辑约束机制研究（2011M500612）</li>
+				<li>4. 吉林省青年科研基金项目：结合模态逻辑知识表示与推理的自动规划方法研究（20140520069JH）</li>
+				<li>5. 中央高校基本科研业务费专项资金——吉林大学科学前沿与交叉学科创新项目：自动规划的逻辑推理特性研究（201103124）</li>
+				<li>6. 中央高校基本科研业务费专项资金——吉林大学种子基金项目：结合模态逻辑知识表示与推理的智能规划方法研究</li>
+				<li>7. 中央高校基本科研业务费专项资金——吉林大学青年教师创新项目：扩展规则推理方法及其在规划推理中的应用研究</li>
+				</ul>
+			</p></div>
+		</div>
+		<div class="content">
+			<div class="profile">张立明</div>
+		</div>
+		<div class="content">
+			<div class="profile">赖永</div>
+			<div class="titles">讲师</div>
+		</div>
+		<div class="school">大连海事大学</div>
+		<div class="content">
+			<div class="profile">高健</div>
+			<div class="titles">副教授</div>
+		</div>
+		<div class="school">武汉大学</div>
+		<div class="content">
+			<div class="profile">玄跻峰</div>
+			<div class="titles">教授</div>
+		</div>
+		<div class="school">武汉工程大学</div>
+		<div class="content">
+			<div class="profile">蔡敦波</div>
+			<div class="titles">副教授</div>
+			<div class="excerpt"><p>
+				主要从事人工智能领域的智能规划(Automated Planning)、自动推理(Automated Reasoning)和约束可满足(Constraint Satisfaction)等方向的研究。探索基于启发式和基于知识的高效问题求解方法，研究复杂人工智能问题的建模、求解与应用。
+				<ul class="project">
+				<li>项目:</li>
+				<li>1. 基于Landmark知识的规划方法研究，国家自然科学基金青年基金项目, 2012-2014。 （主持）</li>
+				<li>2. 开放式规划研究，国家自然科学基金青年基金项目，2009-2011。（参加）</li>
+				</ul>
+			</p></div>
+		</div>
+		<div class="school">大连理工大学</div>
+		<div class="content">
+			<div class="profile">江贺</div>
+			<div class="titles">教授</div>
+		</div>
+		<div class="school">东北师范大学</div>
 		<div class="content">
 			<div class="profile">殷明浩</div>
+			<div class="titles">教授</div>
 			<div class="excerpt"><p>
 				2008年于吉林大学计算机科学与技术学院获工学博士学位，2004年硕士毕业留校工作，2011年6月获博士生导师资格，2013年入选教育部新世纪优秀人才计划，2014年6月聘任为教授。主要从事人工智能、组合优化、理论计算机科学、大数据分析等方面研究。
 				<ul class="project">
@@ -23,21 +170,16 @@
 			</p></div>
 		</div>
 		<div class="content">
-			<div class="profile">李向涛</div>
-			<div class="excerpt"><p>
-				个人主页：http://xiangtaoli.weebly.com/<br>
-				Dr. Li received the M.Eng, and P.HD. degrees in computer science from Northeast Normal University (NENU), Jilin, China, in 2012, and 2015, respectively. Currently, he is an associate professor in School of Computer Science, Northeast Normal University, Jilin, China. His research interests include  evolutionary data mining, bioinformatics, multi-objective optimization and their applications.
-				<ul class="project">
-				<li>论文:</li>
-				<li>1. A particle swarm inspired cuckoo search algorithm for real parameter optimization，SOFT COMPUT，2016年</li>
-				<li>2. Modified differential evolution with self-adaptive parameters method，J COMB OPTIM，2016年</li>
-				<li>3. Multiobjective Local Search Algorithm-Based Decomposition for Multiobjective Permutation Flow Shop Scheduling Problem（SSCI），IEEE TRANSACTIONS ON ENGINEERING MANAGEMENT，2015年</li>
-				<li>4. Modified cuckoo search algorithm with self adaptive parameter method，INFORM SCIENCES，2015年</li>
-				</ul>
-			</p></div>
+			<div class="profile">罗娜</div>
+			<div class="titles">副教授</div>
+		</div>
+		<div class="content">
+			<div class="profile">李宏博</div>
+			<div class="titles">讲师</div>
 		</div>
 		<div class="content">
 			<div class="profile">周俊萍</div>
+			<div class="titles">讲师</div>
 			<div class="excerpt"><p>
 				职　　称：讲师<br>
 				电子邮件：zhoujp877@nenu.edu.cn
@@ -50,6 +192,24 @@
 				</ul>
 			</p></div>
 		</div>
+
+		<div class="school">浙江师范大学</div>
+		<div class="content">
+			<div class="profile">赵相福</div>
+			<div class="titles">副教授</div>
+			<div class="excerpt"><p>
+				IEEE会员、ACM会员、CCF会员，曾任AAAI-15 程序委员、FAW-AAIM2011组委会委员员，以及T-ASE、T-SMCA、KBS、《计算机研究与发展》、《电子学报》、《通信学报》等期刊审稿人及多个科研项目基金评审人。
+				<ul class="project">
+				<li>项目:</li>
+				<li>1. 浙江省自然科学基金一般项目：基于分治策略快速产生大规模极小Hitting-Sets的方法（No. LY16F020004），项目负责人</li>
+				<li>2. 国家自然科学基金（青年科学基金）项目：离散事件系统基于模型的分布式在线诊断方法研究（No. 61003101），项目负责人</li>
+				<li>3. 浙江省自然科学基金一般项目：离散事件系统基于模型的分布式及分层诊断方法研究（No. Y1100191），项目负责人</li>
+				<li>4. 吉林大学研究生“985工程”创新计划项目：离散事件系统的基于模型诊断研究（No. 20080115），项目负责人</li>
+				<li>5. 国家自然科学基金项目：模型不完备情形下离散事件系统诊断方法的研究（No. 60973089），项目主要参与者（3/10）</li>
+				</ul>
+			</p></div>
+		</div>
+
 	</div>
 </template>
 
@@ -57,15 +217,22 @@
 	.professor
 		line-height 2
 		font-size 18px
+		.school
+			background-color #eee
+			padding 0 20px
 		&>.content
+			padding 0 20px
 			min-height 20px
 			display flex
 			&:nth-child(n+1)
-				margin-top 20px
+				margin 10px 0
 			.profile
 				flex 0 0 25%
+			.titles
+				flex 0 0 75%
 			.excerpt
 				flex 0 0 75%
+				display none
 				.project
 					li
 						&:first-child
