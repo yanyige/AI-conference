2e9ac1a14e7f15c131431c370a8cf427fa2dd391
yanyige <232218719@qq.com>
2017-06-01 19:55:16 +0800

    update info in home page and professor

--- src/components/Organization.vue
+++ src/components/Organization.vue
@@ -5,19 +5,22 @@
 	 	</div>
 		<div class="content">
 			<div>
-				<p>主办单位: <p class="indent1"><a href="http://www.ios.ac.cn/wiki/%E9%A6%96%E9%A1%B5">中国科学院软件研究所</a></p><p class="indent1"><a href="http://www.nenu.edu.cn/">东北师范大学</a></p><p class="indent1"><a href="http://www.jlu.edu.cn/">吉林大学</a></p></p>
+				<p>主办单位: <p class="indent1"><a href="http://www.nenu.edu.cn/">东北师范大学</a></p><p class="indent1"><a href="http://www.jlu.edu.cn/">吉林大学</a></p></p>
 			</div>
 			<div>
 				<p>协办单位: 暂无</p>
 			</div>
 			<div>
-				<p>会议主席: 待定</p>
+				<p>组织委员会: </p>
+				<p class="indent1">蔡少伟（中科院软件所）</p>
+				<p class="indent1">殷明浩（东北师范大学）</p>
+				<p class="indent1">吕志鹏（华中科技大学）</p>
  			</div>
  			<div>
-				<p>[组委会联系方式]: <p class="indent1">通信地址：吉林省长春市南关区净月经济开发区净月大街2555号 计算机科学与技术学院</p></p>
+				<p>[组委会联系方式]: <p class="indent1">通信地址：吉林省长春市南关区净月经济开发区净月大街2555号 信息科学与技术学院</p></p>
  			</div>
  			<p>邮编：130117</p>
- 			<p>yinmh@nenu.edu.cn</p>
+ 			<p>ymh@nenu.edu.cn</p>
 		</div>
 	</div>
 </template>
