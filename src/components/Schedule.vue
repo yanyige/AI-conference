da4f42500ca378c485df3a5b9cd7d850825d1385
严一格 <yanyige@yanyigedeMacBook-Pro.local>
2017-06-29 22:56:08 +0800

    update info

--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -1,9 +1,155 @@
 <template>
 	<div class="schedule">
 	 	<p class="title">7月6日 星期四</p>
-		<p class="para">一个美好的周四~</p>
-		<p class="title">7月7日（半天） 星期五</p>
-		<p class="para">下午自由交流</p>
+		<p class="info">主持：李占山</p>
+		<div class="schedule-content">
+			<div class="schedule-content-left">
+				<p>9:00 - 9:45</p>
+			</div>
+			<div class="schedule-content-right">
+				<p>Incrementality in exact NP-hard problem solving</p>
+				<p>李初民</p>  
+			</div>		
+		</div>
+
+		<div class="schedule-content">
+			<div class="schedule-content-left">
+				<p>9:45 - 10:15</p>
+			</div>
+			<div class="schedule-content-right">
+				<p>光网络设计与路径规划：模型与算法</p>
+				<p>吕志鹏</p>
+			</div>		
+		</div>
+		<div class="schedule-content">
+			<div class="schedule-content-left">
+				<p>10:15 - 10:45</p>
+			</div>
+			<div class="schedule-content-right">
+				<p style="color: grey">茶歇</p>
+			</div>		
+		</div>
+		<p class="info">主持：吕志鹏</p>
+		<div class="schedule-content">
+			<div class="schedule-content-left">
+				<p>10:45 - 11:15</p>
+			</div>
+			<div class="schedule-content-right">
+				<p>程序修复中的约束求解研究</p>
+				<p>玄跻峰</p>
+			</div>		
+		</div>
+		<div class="schedule-content">
+			<div class="schedule-content-left">
+				<p>11:15 - 11:45</p>
+			</div>
+			<div class="schedule-content-right">
+				<p>分组密码分析中的组合优化问题</p>
+				<p>孙思维</p>
+			</div>		
+		</div>
+		<p class="info">主持：李初民</p>
+		<div class="schedule-content">
+			<div class="schedule-content-left">
+				<p>14:30 - 15:00</p>
+			</div>
+			<div class="schedule-content-right">
+				<p>Heuristics for SAT/MaxSAT</p>
+				<p>蔡少伟</p>
+			</div>		
+		</div>
+		<div class="schedule-content">
+			<div class="schedule-content-left">
+				<p>15:00 - 15:30</p>
+			</div>
+			<div class="schedule-content-right">
+				<p>Branch and price based metaheuristic</p>
+				<p>柯良军</p>
+			</div>		
+		</div>
+		<div class="schedule-content">
+			<div class="schedule-content-left">
+				<p>15:30 - 16:00</p>
+			</div>
+			<div class="schedule-content-right">
+				<p>玉兔号月面巡视器任务规划模型和算法</p>
+				<p>蔡敦波</p>
+			</div>		
+		</div>
+		<div class="schedule-content">
+			<div class="schedule-content-left">
+				<p>16:00 - 16:30</p>
+			</div>
+			<div class="schedule-content-right">
+				<p style="color: grey">茶歇</p>
+			</div>		
+		</div>
+		<p class="info">主持：Jimmy Lee</p>
+		<div class="schedule-content">
+			<div class="schedule-content-left">
+				<p>16:30 - 17:00</p>
+			</div>
+			<div class="schedule-content-right">
+				<p>基于知识编译的近似模型计数：初步结果</p>
+				<p>赖永</p>
+			</div>		
+		</div>
+		<div class="schedule-content">
+			<div class="schedule-content-left">
+				<p>17:00 - 17:30</p>
+			</div>
+			<div class="schedule-content-right">
+				<p>A New Probabilistic Algorithm for Approximate Model Counting</p>
+				<p>葛存青</p>
+			</div>		
+		</div>
+		<p class="title">7月7日 星期五</p>
+		<p class="info">主持：江贺</p>
+		<div class="schedule-content">
+			<div class="schedule-content-left">
+				<p>9:00 - 9:45</p>
+			</div>
+			<div class="schedule-content-right">
+				<p>Global Cost Functions in Weighted Constraint Satisfaction</p>
+				<p>Jimmy Lee</p>
+			</div>		
+		</div>
+		<div class="schedule-content">
+			<div class="schedule-content-left">
+				<p>9:45 - 10:15</p>
+			</div>
+			<div class="schedule-content-right">
+				<p>A Parameterized Approach to Heterogeneous Data Analysis of Otitis Media Treatment Data</p>
+				<p>Peter Shaw</p>
+			</div>		
+		</div>
+		<div class="schedule-content">
+			<div class="schedule-content-left">
+				<p>10:15 - 10:45</p>
+			</div>
+			<div class="schedule-content-right">
+				<p style="color: grey">茶歇</p>
+			</div>		
+		</div>
+		<p class="info">主持：吕帅</p>
+		<div class="schedule-content">
+			<div class="schedule-content-left">
+				<p>10:45 - 11:15</p>
+			</div>
+			<div class="schedule-content-right">
+				<p>特殊二部图及其算法</p>
+				<p>刘田</p>
+			</div>		
+		</div>
+		<div class="schedule-content">
+			<div class="schedule-content-left">
+				<p>11:15 - 11:45</p>
+			</div>
+			<div class="schedule-content-right">
+				<p>二部图完美匹配的局部性</p>
+				<p>刘兴武</p>
+			</div>		
+		</div>
 	</div>
 </template>
 
@@ -14,7 +160,20 @@
 		.title
 			color blue
 			text-align center
-			font-size 24px
+			font-size 28px
+		.info
+			background-color rgb(238, 238, 238)
+			text-align center
+			font-weight bold
+		.schedule-content
+			display flex
+			.schedule-content-left
+				text-align center
+				float left
+				flex 0 0 20%
+			.schedule-content-right
+				p:first-child
+					color red
 		.para
 			text-indent 2em
 		.mail
